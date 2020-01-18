46ced6bc53537814ce98909371d3292d12f4d693
m-zia <51455430+m-zia@users.noreply.github.com>
2020-01-18 17:54:20 +0000

    Update Youtube Embed.js
    Started using jQuery.
    
    Function now works when user presses enter on the form.
    
    Added functions to handle the case when users enter a shortcut youtube URL

--- Youtube Embed.js
+++ Youtube Embed.js
@@ -1,19 +1,80 @@
 
-// /main function
 
+//run the embed function when the button is clicked
+$("#mybutton").click(embed);
+
+
+// function when user presses the button
 function embed() {
+
 	var url = document.getElementById('url').value 
 
-	var url_section = url.split(/=(.+)/)[1];
+	// if the user inputs the full youtube url
+	if (url.includes("=")) {
+		var url_section = url.split(/=(.+)/)[1];
 
-	var link = "https://www.youtube.com/embed/" + url_section;
+		var link = "https://www.youtube.com/embed/" + url_section;
     
-	document.getElementById('myframe').src = link;
+		document.getElementById('myframe').src = link;
+	} 
 
+	// if the user inputs the shotcut youtube url
+	else if (!(url.includes("="))) {
+		var urlSections = url.split("/");
+				
+		var urlEnd = urlSections[urlSections.length - 1];
+	
+		var link = "https://www.youtube.com/embed/" + urlEnd;
+
+   
+		document.getElementById('myframe').src = link;
+	}
 }
 
-document.getElementById("mybutton").addEventListener("click", embed);
 
 
+// prevent the form from refreshing the page when it submits anything 
+// we're stopping the default action
+$('form').submit(function (event) {
+    event.preventDefault();
+});
+
+
+// when user presses enter in the form, run the function
+$("input").on("keypress", function(event) {
+	if (event.keyCode === 13) {  //checks whether the pressed key is "Enter"
+		var url_in_form = (event.delegateTarget.value); // get the url in the form
+		embed2(url_in_form)
+    }
+})
+
+
+// function that runs when user presses enter in the form
+function embed2(x) {
+
+	// if the user inputs the full youtube url
+	if (x.includes("=")) {
+		var url = " " + x ;
+
+		var url_section = url.split(/=(.+)/)[1]; //only want the part of the url that comes after the "="
+
+		var link = "https://www.youtube.com/embed/" + url_section;
+    
+		document.getElementById('myframe').src = link;	
+	} 
+
+	// if the user inputs the shortcut youtube url
+	else if (!(x.includes("="))) {
+
+		var urlSections = x.split("/");
+				
+		var urlEnd = urlSections[urlSections.length - 1];
+	
+		var link = "https://www.youtube.com/embed/" + urlEnd;
+
+		document.getElementById('myframe').src = link;
+	}
+	
+}
 
 
